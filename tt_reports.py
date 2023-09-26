bc9e1b7d0c842fcde68d6e4af9eb75af317ef390
Todd Glover <emerson.glover@gmail.com>
2023-09-26 14:19:16 -0700

    basic "notes" functionality

--- tt_reports.py
+++ tt_reports.py
@@ -31,6 +31,7 @@ from tt_event import Event
 import tt_block
 import tt_printer as pr
 import tt_conf as cfg
+import tt_notes as notes
 
 # try:
 #    import tt_local_config  # pylint:disable=unused-import
@@ -348,6 +349,8 @@ def audit_report(day: TrackerDay, args: list[str]) -> None:
         pr.iprint(line)
     if not prefixes_returned_out:
         pr.iprint("-no bikes-")
+
+    notes_bit(day)
 
     return
 
@@ -868,6 +871,18 @@ def day_end_report(day: TrackerDay, args: list) -> None:
     visit_lengths_by_category_report(visits)
     visit_statistics_report(visits)
 
+    notes_bit(day)
+
+def notes_bit(day:TrackerDay) -> None:
+    """Add a 'notes' section to a report."""
+    pr.iprint()
+
+    pr.iprint("Today's notes:",style=cfg.TITLE_STYLE)
+    if not day.notes:
+        for line in day.notes:
+            pr.iprint(line,style=cfg.NORMAL_STYLE,num_indents=1)
+    else:
+        pr.iprint("There are no notes yet today.",num_indents=2)
 
 def busyness_report(day: TrackerDay, args: list) -> None:
     """Report more summary statistics about visits, up to the given time.
