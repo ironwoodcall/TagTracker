c31ded33b99d28557f9927d5a1287a7fb4c01984
tevpg <tevpgx@gmail.com>
2025-09-19 16:40:38 -0700

    use standard rather than quick estimation model

--- web/web_day_detail.py
+++ web/web_day_detail.py
@@ -530,7 +530,7 @@ def summary_table(
 
     the_estimate = None
     if is_today:
-        est = web_estimator.Estimator(estimation_type="QUICK")
+        est = web_estimator.Estimator(estimation_type="STANDARD")
         est.guess()
         if est.state != web_estimator.ERROR and est.time_closed > VTime("now"):
             est_min = est.bikes_so_far + est.min
